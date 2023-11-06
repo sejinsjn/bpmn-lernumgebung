497a8c6545b6b38f0511b16e6872a106391e8559
sejinsjn <sedad_@hotmail.de>
2023-11-06 12:43:00 +0100

    open close element list in task

--- src/components/beschriftungen.js
+++ src/components/beschriftungen.js
@@ -1,40 +1,59 @@
 import styles from './beschriftungen.module.css';
+import React, { useState } from 'react';
 
 const Beschriftungen = ({ParsedSolution}) => {
+  const [showBpmnElements, setShowBpmnElements] = useState(true);
+  const [showLanes, setShowLanes] = useState(true);
+  const [showParticipants, setShowParticipants] = useState(true);
 
     return (
       <>
         <div className={styles.container}>
-          <p className={styles.header}>BPMN Elemente</p>
-          <div className={styles.feedbackElements}>
-            {ParsedSolution.processes.bpmnElementsArray.map((item, index) => (
-              item.getAttribute('name') && <div key={index} style={{border: '1px solid black', textAlign: 'center', margin: '5px auto', borderRadius: '20px', fontSize: '14px', padding: '5px'}}>
-                <p style={{margin: '0'}}>{item.nodeName.replace("bpmn:", "")}</p>
-                <p style={{margin: '0'}}>{item.getAttribute('name')}</p>
-              </div>
-            ))}
+          <div className={styles.toggle} onClick={() => setShowBpmnElements(!showBpmnElements)}>
+            <span>BPMN Elemente</span> 
+            <i className={`${styles.arrow} ${showBpmnElements ? styles.up : styles.down}`}></i>
+          </div>
+          <div className={`${styles.content} ${showBpmnElements ? styles.open : ''}`}>
+            <div className={styles.feedbackElements}>
+              {ParsedSolution.processes.bpmnElementsArray.map((item, index) => (
+                item.getAttribute('name') && <div key={index} style={{border: '1px solid black', textAlign: 'center', margin: '5px auto', borderRadius: '20px', fontSize: '14px', padding: '5px'}}>
+                  <p style={{margin: '0'}}>{item.nodeName.replace("bpmn:", "")}</p>
+                  <p style={{margin: '0'}}>{item.getAttribute('name')}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
         <div className={styles.container}>
-          <p className={styles.header}>Lanes</p>
-          <div className={styles.feedbackElements}>
-            {ParsedSolution.processes.laneSets.map((item, index) => (
-              item.getAttribute('name') && <div key={index} style={{border: '1px solid black', textAlign: 'center', margin: '5px auto', borderRadius: '20px', fontSize: '14px', padding: '5px'}}>
-                <p style={{margin: '0'}}>{item.nodeName.replace("bpmn:", "")}</p>
-                <p style={{margin: '0'}}>{item.getAttribute('name')}</p>
-              </div>
-            ))}
+          <div className={styles.toggle} onClick={() => setShowLanes(!showLanes)}>
+            <span>Lanes</span> 
+            <i className={`${styles.arrow} ${showLanes ? styles.up : styles.down}`}></i>
+          </div>
+          <div className={`${styles.content} ${showLanes ? styles.open : ''}`}>
+            <div className={styles.feedbackElements}>
+              {ParsedSolution.processes.laneSets.map((item, index) => (
+                item.getAttribute('name') && <div key={index} style={{border: '1px solid black', textAlign: 'center', margin: '5px auto', borderRadius: '20px', fontSize: '14px', padding: '5px'}}>
+                  <p style={{margin: '0'}}>{item.nodeName.replace("bpmn:", "")}</p>
+                  <p style={{margin: '0'}}>{item.getAttribute('name')}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
         <div className={styles.container}>
-          <p className={styles.header}>Akteure</p>
-          <div className={styles.feedbackElements}>
-            {ParsedSolution.collaborations.participants.map((item, index) => (
-              item.getAttribute('name') && <div key={index} style={{border: '1px solid black', textAlign: 'center', margin: '5px auto', borderRadius: '20px', fontSize: '14px', padding: '5px'}}>
-                <p style={{margin: '0'}}>{item.nodeName.replace("bpmn:", "")}</p>
-                <p style={{margin: '0'}}>{item.getAttribute('name')}</p>
-              </div>
-            ))}
+          <div className={styles.toggle} onClick={() => setShowParticipants(!showParticipants)}>
+            <span>Akteure</span> 
+            <i className={`${styles.arrow} ${showParticipants ? styles.up : styles.down}`}></i>
+          </div>
+          <div className={`${styles.content} ${showParticipants ? styles.open : ''}`}>
+            <div className={styles.feedbackElements}>
+              {ParsedSolution.collaborations.participants.map((item, index) => (
+                item.getAttribute('name') && <div key={index} style={{border: '1px solid black', textAlign: 'center', margin: '5px auto', borderRadius: '20px', fontSize: '14px', padding: '5px'}}>
+                  <p style={{margin: '0'}}>{item.nodeName.replace("bpmn:", "")}</p>
+                  <p style={{margin: '0'}}>{item.getAttribute('name')}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </>
